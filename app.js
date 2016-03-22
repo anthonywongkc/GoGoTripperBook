827b018a5c04e963ffaa7358a7023703b4548855
Anthony Wong <anthony_kc_wong@hotmail.com>
2016-03-23 00:17:38 +0800

    solve key problem

--- app.js
+++ app.js
@@ -19,14 +19,18 @@ app.set('view engine', 'jade');
 
 //stormpath for user login
 app.use(stormpath.init(app, {
-
+    apiKeyFile: './.stormpath/apiKey-2QJ7LR50XMN7ZC4Q4FMM6M49C.properties',
+    secretKey: 'some_random_long_string_here',
+    application: 'https://api.stormpath.com/v1/applications/5gW8aKppk8lSg2MUEdvWX',
 }));
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -38,10 +42,10 @@ app.get('/plans', routes.plans);
 app.get('/about', routes.about);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
@@ -49,23 +53,23 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
 
