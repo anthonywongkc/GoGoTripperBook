af0f580fc02500efea49fbf7794139ba353537c3
Terence Wu <terenceccwu@gmail.com>
2016-03-23 01:17:52 +0800

    added draft of planner

--- app.js
+++ app.js
@@ -18,11 +18,11 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 //stormpath for user login
-app.use(stormpath.init(app, {
-    apiKeyFile: './.stormpath/apiKey-2QJ7LR50XMN7ZC4Q4FMM6M49C.properties',
-    secretKey: 'some_random_long_string_here',
-    application: 'https://api.stormpath.com/v1/applications/5gW8aKppk8lSg2MUEdvWX',
-}));
+// app.use(stormpath.init(app, {
+//     apiKeyFile: './.stormpath/apiKey-2QJ7LR50XMN7ZC4Q4FMM6M49C.properties',
+//     secretKey: 'some_random_long_string_here',
+//     application: 'https://api.stormpath.com/v1/applications/5gW8aKppk8lSg2MUEdvWX',
+// }));
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -34,12 +34,28 @@ app.use(bodyParser.urlencoded({
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// for API call use
+// app.use(function (req, res, next) {
+//     // Website you wish to allow to connect e.g. http://localhost:3000
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     // Request methods you wish to allow
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//     // Request headers you wish to allow
+//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+//     // Set to true if you need the website to include cookies in the requests sent
+//     // to the API (e.g. in case you use sessions)
+//     res.setHeader('Access-Control-Allow-Credentials', true);
+//     // Pass to next layer of middleware
+//     next();
+// });
+
 ////////////////////////////////
 // Routing Pages to functions //
 ////////////////////////////////
 app.get('/', routes.index);
 app.get('/plans', routes.plans);
 app.get('/about', routes.about);
+app.get('/planner', routes.planner);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
